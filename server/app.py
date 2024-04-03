051289414ea760622bb9b16c4513cf3e33f0a3e3
Ian Kinuthia 254 <144425644+KinuthiaIan@users.noreply.github.com>
2024-04-03 20:02:07 +0300

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# Routing views using the @app.route decorator
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+# Variable Routes
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Running the Flask Development server using the app.run() method
+if __name__ == '__main__':
+    app.run(port=5555)
